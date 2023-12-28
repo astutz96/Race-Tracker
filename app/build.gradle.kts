f1effd278f29a05f2aedcbdcce3c95913f1a3128
austinstutzman <alsghs49@gmail.com>
2023-12-28 16:11:50 -0500

    updating sdk & gradle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,12 +20,12 @@ plugins {
 
 android {
     namespace = "com.example.racetracker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.racetracker"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
